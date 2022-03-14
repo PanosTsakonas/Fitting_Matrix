ac46afe97bf244cf50092eca7a01a3d7a1bb8510
Panos <64256997+PanosTsakonas@users.noreply.github.com>
2022-03-14 12:24:45 +0000

    Update Fit.m

--- Fit.m
+++ Fit.m
@@ -1,21 +1,21 @@
-%This function calculates the fitted parameters of the MoCap data of the spring like motion of the digit segments based on the analytical solutions of the IBK model
-
+%This function calculates the fitted parameters for the underdamped case of
+%the IBK model
 clc;
 close all;
 
 %Set the sampling frequency of the motion capture system
-fs=150;
+fs=200;
 
+%Uncomment the following when I am working from my Laptop
+I=importfile("%%%% Specify the directory of the template.xlsx %%%","");
 
-I=importfile("%%%% SPECIFY THE DIRECTORY OF THE EXCEL FILE %%%%%%%%.xlsx","");
-
-b=input("Specify the digit you are working on: ");
+in=input("Specify the digit you are working on: ");
 %1 for thumb, 2 for index etc.
 
-Imcp=I.data(27+b,4);
-Ipip=I.data(27+b,5);
-Idip=I.data(27+b,6);
-Ia=I.data(27+b,7);
+Imcp=I.data(27+in,4);
+Ipip=I.data(27+in,5);
+Idip=I.data(27+in,6);
+Ia=I.data(27+in,7);
 
 
 %The 20 Hz frequency was taken from " Biodynamic modeling,
@@ -36,11 +36,14 @@ th2=th2.*pi/180;
 th3=th3.*pi/180;
 tha=tha.*pi/180;
 
-%Specify the number of frames you want for flexion of MCP and PIP joints. 
+%Specify the number of frames you want for flexion of PIP and MCP joints. 
 n=50;
 
+%Specify frames for DIP joint flexion
+n3_1=40;
+
 %Specify the number of frames you want for abduction
-nab=110;
+nab=130;
 
 %Time is the same for flexion trials starting from 0 and ends after n frames
 t=zeros(1,n);
@@ -48,29 +51,25 @@ for i=2:n
     t(i)=t(i-1)+1/fs;
 end
 
-% Specify  the frames for the DIP joint
-n3_1=50;
 
-% Time for DIP joint
 t3=zeros(1,n3_1);
 for i=2:n3_1
     t3(i)=t(i-1)+1/fs;
 end
 
-% Time for abduction trials
+%Time for abduction trials
 ta=zeros(1,nab);
 for i=2:nab
     ta(i)=ta(i-1)+1/fs;
 end
-
-% Identify the peaks of the signals. Chage the values where appropriate!
+%Identify the peaks of the signals
 [p1,n1]=findpeaks(th1,'MINPEAKPROMINENCE',4*pi/180);
 [p2,n2]=findpeaks(th2,'MINPEAKPROMINENCE',10*pi/180);
 [p3,n3]=findpeaks(th3,'MINPEAKPROMINENCE',13*pi/180);
-[pa,na]=findpeaks(-tha,'MINPEAKWIDTH',50); %For the abduction movement since the digit is moved towards the midline (smaller angles) the peaks are the found from the -tha matrix
+[pa,na]=findpeaks(-tha,'MINPEAKWIDTH',15);
  
 
-%Specify the fitting equations
+%fitting equations
 fitfun1=fittype(@(a,b,c,x) a+c.*exp(-b.*x)); %Overdamped
 fitfun2=fittype(@(a,b,c,x) a+c.*exp(-b.*x).*(1+b.*x)); %Critically damped
 fitfun=fittype(@(a,b,c,d,w,x) a+c.*exp(-b.*x).*(d.*sin(w.*x)+cos(w.*x)));%Underdamped
@@ -97,14 +96,227 @@ Ypip=zeros(n,length(n2));
 Ydip=zeros(n3_1,length(n3));
 Ya=zeros(nab,length(na));
 
+
+%If you are working on the thumb
+if in==1
+
+%For the thumb digit the Imcp corresponds to the CMC segment 
+%the Ipip corresponds to the Proximal phalanx and 
+%the Idip corresponds to the Distal phalanx moment of inertia respectivelly.
+ 
+ %Create a waitbar functionality
+f = waitbar(0,'1','Name','Curve fitting...',...
+    'CreateCancelBtn','setappdata(gcbf,''canceling'',1)');
+
+setappdata(f,'canceling',0);
+
+%MCP Joint
+for i=1:length(n2)-1
+    
+% Update waitbar and message
+waitbar(i/length(n2),f,sprintf('Middle thumb segment data fitting at %d%s',floor((i/length(n2))*100),'%'));
+% Check for clicked Cancel button
+if getappdata(f,'canceling')
+    break
+end
+      
+%Starting point for underdamped case
+x0=[th2(n2(i)+n-1) 20 th2(n2(i))-th2(n2(i)+n-1) 0.2 10];
+[Fit,GoF]=fit(transpose(t),th2(n2(i):n2(i)+n-1),fitfun,'MaxFunEvals',10^6,'MaxIter',10^6,'StartPoint',x0,'ToLFun',10^-8);
+Ymcp(:,i)= Fit(t);
+coef=coeffvalues(Fit);
+Bmcp(i)=2*Ipip*coef(2);
+syms K
+eqn={abs(coef(5))==sqrt(4*K*Ipip-Bmcp(i)^2)/(2*Ipip)};
+Kmcp(i)=eval(solve(eqn,K));
+theqmcp(i)=coef(1);
+th0mcp(i)=coef(3)+theqmcp(i);
+figure(i)
+plot(t,Fit(t).*180/pi,t,th2(n2(i):n2(i)+n-1).*180/pi);
+legend("Fitted curve","Data from index "+n2(i));
+xlabel("Time (s)");
+ylabel("Angle (degrees)");
+title("MCP joint fit with RMSE: "+(GoF.rmse*180/pi)+" and R^2: "+GoF.rsquare);
+clear Fit Fit1 Fit2 GoF GoF1 GoF2;
+end
+
+
+delete(f);
+
 %Create a waitbar functionality
 f = waitbar(0,'1','Name','Curve fitting...',...
     'CreateCancelBtn','setappdata(gcbf,''canceling'',1)');
 
 setappdata(f,'canceling',0);
 
-% MCP Joint
-for i=1:length(n1)-1
+%IP Joint
+for j=1:length(n3)
+% Update waitbar and message
+waitbar(j/length(n3),f,sprintf('Distal thumb segment data fitting at %d%s',floor((j/length(n3))*100),'%'));
+    
+% Check for clicked Cancel button
+if getappdata(f,'canceling')
+ break
+end
+
+%Starting point for underdamped case
+x0=[th3(n3(j)+n-1) 30 th3(n3(j))-th3(n3(j)+n3_1-1) 0.2 20];
+x01=[th3(n3(j)+n-1) 30 th3(n3(j))-th3(n3(j)+n3_1-1)];
+[Fit,GoF]=fit(transpose(t3),th3(n3(j):n3(j)+n3_1-1),fitfun,'MaxFunEvals',10^6,'MaxIter',10^6,'StartPoint',x0,'ToLFun',10^-8);
+[Fit1,GoF1]=fit(transpose(t3),th3(n3(j):n3(j)+n3_1-1),fitfun1,'MaxFunEvals',10^6,'MaxIter',10^6,'StartPoint',x01,'ToLFun',10^-8);
+[Fit2,GoF2]=fit(transpose(t3),th3(n3(j):n3(j)+n3_1-1),fitfun2,'MaxFunEvals',10^6,'MaxIter',10^6,'StartPoint',x01,'ToLFun',10^-8);
+GoF_rmse=min([GoF.rmse GoF1.rmse GoF2.rmse]);
+if GoF_rmse==GoF.rmse
+   % disp("Underdamped case");
+Ypip(:,j)=Fit(t3);
+coef=coeffvalues(Fit);
+Bpip(j)=2*Idip*coef(2);
+syms K
+eqn={abs(coef(5))==sqrt(4*K*Idip-Bpip(j)^2)/(2*Idip)};
+Kpip(j)=eval(solve(eqn,K));
+theqpip(j)=coef(1);
+th0pip(j)=coef(3)+theqpip(j);
+figure(j+i-1)
+plot(t3,Fit(t3).*180/pi,t3,th3(n3(j):n3(j)+n3_1-1).*180/pi);
+legend("Fitted curve","Data from index "+n3(j));
+xlabel("Time (s)");
+ylabel("Angle (degrees)");
+title("IP joint fit with RMSE: "+(GoF.rmse*180/pi)+" and R^2: "+GoF.rsquare);
+elseif GoF_rmse==GoF1.rmse
+    %disp("Overdamped Case");
+    Ypip(:,j)=Fit1(t3);
+coef=coeffvalues(Fit1);
+b=coef(2);
+c=coef(3);
+rf=b*c/(c+(coef(1)-th3(n3(j))));
+Kpip(j)=rf*b*Idip;
+Bpip(j)=abs(rf+b)*Idip;
+theqpip(j)=coef(1);
+th0pip(j)=th2(n3(j));
+figure(j+i-1)
+plot(t3,Fit1(t3).*180/pi,t3,th3(n3(j):n3(j)+n3_1-1).*180/pi);
+legend("Fitted curve","Data from index "+n3(j));
+xlabel("Time (s)");
+ylabel("Angle (degrees)");
+title("IP joint fit with RMSE: "+(GoF1.rmse*180/pi)+" and R^2: "+GoF1.rsquare);
+else
+    %disp("Critically damped case");
+    Ypip(:,j)=Fit2(t3);
+    coef=coeffvalues(Fit2);
+    Bpip(j)=2*Idip*coef(2);
+    syms K
+    eqn={Bpip(j)^2-4*K*Idip==0};
+    Kpip(j)=eval(solve(eqn,K));
+    theqpip(j)=coef(1);
+th0pip(j)=th3(n3(j));
+figure(j+i-1)
+plot(t3,Fit2(t3).*180/pi,t3,th3(n3(j):n3(j)+n3_1-1).*180/pi);
+legend("Fitted curve","Data from index "+n3(j));
+xlabel("Time (s)");
+ylabel("Angle (degrees)");
+title("IP joint fit with RMSE: "+(GoF2.rmse*180/pi)+" and R^2: "+GoF2.rsquare);
+end
+clear Fit Fit1 Fit2 GoF GoF1 GoF2;
+end
+delete(f);
+
+%Create a waitbar functionality
+f = waitbar(0,'1','Name','Curve fitting...',...
+    'CreateCancelBtn','setappdata(gcbf,''canceling'',1)');
+
+setappdata(f,'canceling',0);
+
+%Abduction
+for k=1:length(na)
+% Update waitbar and message
+waitbar(k/length(na),f,sprintf('Thumb abduction data fitting at %d%s',floor((k/length(na))*100),'%'));
+
+% Check for clicked Cancel button
+if getappdata(f,'canceling')
+    break
+end
+%Starting point for underdamped case
+x0=[tha(na(k)+nab-1) 20 tha(na(k))-tha(na(k)+nab-1) 0.2 30];
+x01=[tha(na(k)+nab-1) 20 tha(na(k))-tha(na(k)+nab-1)];
+[Fit,GoF]=fit(transpose(ta),tha(na(k):na(k)+nab-1),fitfun,'MaxFunEvals',10^6,'MaxIter',10^6,'StartPoint',x0,'ToLFun',10^-8);
+[Fit1,GoF1]=fit(transpose(ta),tha(na(k):na(k)+nab-1),fitfun1,'MaxFunEvals',10^6,'MaxIter',10^6,'StartPoint',x01,'ToLFun',10^-8);
+[Fit2,GoF2]=fit(transpose(ta),tha(na(k):na(k)+nab-1),fitfun2,'MaxFunEvals',10^6,'MaxIter',10^6,'StartPoint',x01,'ToLFun',10^-8);
+GoF_rmse=min([GoF.rmse GoF1.rmse GoF2.rmse]);
+
+if GoF_rmse==GoF.rmse
+%disp("Underdamped Case");
+Ya(:,k)=Fit(ta);
+coef=coeffvalues(Fit);
+Ba(k)=2*Ia*coef(2);
+syms K
+eqn={abs(coef(5))==sqrt(4*K*Ia-Ba(k)^2)/(2*Ia)};
+Ka(k)=eval(solve(eqn,K));
+theqa(k)=coef(1);
+th0a(k)=coef(3)+theqa(k);
+figure(j+i+k-2)
+plot(ta,Fit(ta).*180/pi,ta,tha(na(k):na(k)+nab-1).*180/pi);
+legend("Fitted curve","Data from index "+na(k));
+xlabel("Time (s)");
+ylabel("Angle (degrees)");
+title("Thumb abduction fit with RMSE: "+(GoF.rmse*180/pi)+" and R^2: "+GoF.rsquare);
+elseif GoF_rmse==GoF1.rmse
+   % disp("Overdamped Case");
+    Ya(:,k)=Fit1(ta);
+coef=coeffvalues(Fit1);
+b=coef(2);
+c=coef(3);
+rf=b*c/(c+(coef(1)-tha(na(k))));
+Ka(k)=rf*b*Ia;
+Ba(k)=abs(rf+b)*Ia;
+theqa(k)=coef(1);
+th0a(k)=tha(na(k));
+figure(j+i+k-2)
+plot(ta,Fit1(ta).*180/pi,ta,tha(na(k):na(k)+nab-1).*180/pi);
+legend("Fitted curve","Data from index "+na(k));
+xlabel("Time (s)");
+ylabel("Angle (degrees)");
+title("Thumb abduction fit with RMSE: "+(GoF1.rmse*180/pi)+" and R^2: "+GoF1.rsquare);
+else
+    %disp("Critically damped case");
+    Ya(:,k)=Fit2(ta);
+    coef=coeffvalues(Fit2);
+    Ba(k)=2*Ia*coef(2);
+    syms K
+    eqn={Ba(k)^2-4*K*Ia==0};
+    Ka(k)=eval(solve(eqn,K));
+    theqa(k)=coef(1);
+th0a(k)=tha(na(k));
+figure(j+i+k-2)
+plot(ta,Fit2(ta).*180/pi,ta,tha(na(k):na(k)+nab-1).*180/pi);
+legend("Fitted curve","Data from index "+na(k));
+xlabel("Time (s)");
+ylabel("Angle (degrees)");
+title("Thumb abduction fit with RMSE: "+(GoF2.rmse*180/pi)+" and R^2: "+GoF2.rsquare);
+end
+clear Fit Fit1 Fit2 GoF GoF1 GoF2;
+end
+
+delete(f);
+
+disp("Mean MCP K: "+mean(Kmcp)+" +/- "+std(Kmcp));
+disp("Mean MCP B: "+mean(Bmcp)+" +/- "+std(Bmcp));
+disp("Mean IP K: "+mean(Kpip)+" +/- "+std(Kpip));
+disp("Mean IP B: "+mean(Bpip)+" +/- "+std(Bpip));
+disp("Mean Abduction K: "+mean(Ka)+" +/- "+std(Ka));
+disp("Mean Abduction B: "+mean(Ba)+" +/- "+std(Ba));
+
+
+
+else %This is for digits index through little
+
+%Create a waitbar functionality
+f = waitbar(0,'1','Name','Curve fitting...',...
+    'CreateCancelBtn','setappdata(gcbf,''canceling'',1)');
+
+setappdata(f,'canceling',0);
+
+%MCP Joint
+for i=1:length(n1)
     
 % Update waitbar and message
 waitbar(i/length(n1),f,sprintf('Proximal segment data fitting at %d%s',floor((i/length(n1))*100),'%'));
@@ -113,7 +325,7 @@ if getappdata(f,'canceling')
     break
 end
       
-% Starting point for underdamped case
+%Starting point for underdamped case
 x0=[th1(n1(i)+n-1) 20 th1(n1(i))-th1(n1(i)+n-1) 0.2 10];
 [Fit,GoF]=fit(transpose(t),th1(n1(i):n1(i)+n-1),fitfun,'MaxFunEvals',10^6,'MaxIter',10^6,'StartPoint',x0,'ToLFun',10^-8);
 Ymcp(:,i)= Fit(t);
@@ -160,7 +372,7 @@ x01=[th2(n2(j)+n-1) 30 th2(n2(j))-th2(n2(j)+n-1)];
 [Fit2,GoF2]=fit(transpose(t),th2(n2(j):n2(j)+n-1),fitfun2,'MaxFunEvals',10^6,'MaxIter',10^6,'StartPoint',x01,'ToLFun',10^-8);
 GoF_rmse=min([GoF.rmse GoF1.rmse GoF2.rmse]);
 if GoF_rmse==GoF.rmse
-  
+   % disp("Underdamped case");
 Ypip(:,j)=Fit(t);
 coef=coeffvalues(Fit);
 Bpip(j)=2*Ipip*coef(2);
@@ -176,7 +388,7 @@ xlabel("Time (s)");
 ylabel("Angle (degrees)");
 title("PIP joint fit with RMSE: "+(GoF.rmse*180/pi)+" and R^2: "+GoF.rsquare);
 elseif GoF_rmse==GoF1.rmse
-  
+    %disp("Overdamped Case");
     Ypip(:,j)=Fit1(t);
 coef=coeffvalues(Fit1);
 b=coef(2);
@@ -193,7 +405,7 @@ xlabel("Time (s)");
 ylabel("Angle (degrees)");
 title("PIP joint fit with RMSE: "+(GoF1.rmse*180/pi)+" and R^2: "+GoF1.rsquare);
 else
-    
+    %disp("Critically damped case");
     Ypip(:,j)=Fit2(t);
     coef=coeffvalues(Fit2);
     Bpip(j)=2*Ipip*coef(2);
@@ -219,8 +431,10 @@ f = waitbar(0,'1','Name','Curve fitting...',...
 
 setappdata(f,'canceling',0);
 
+
 %DIP Joint
-for k=1:length(n3)
+for k=1:length(n3)-1
+    
 % Update waitbar and message
 waitbar(k/length(n3),f,sprintf('Distal segment data fitting at %d%s',floor((k/length(n3))*100),'%'));
 
@@ -228,16 +442,49 @@ waitbar(k/length(n3),f,sprintf('Distal segment data fitting at %d%s',floor((k/le
 if getappdata(f,'canceling')
     break
 end
+
+%For the DIP joint if the occupied bandwidth function returns a value greater than 20 Hz 
+%then that value will be used as the low pass cut off frequency of the filter.
+%For more information about the obw type help obw
+
+if (obw(dip(n3(k):n3(k)+2*n3_1),fs)>20)
+[bm,am]=butter(4,obw(dip(n3(k):n3(k)+2*n3_1),fs)/fs,'low');
+th3f=filtfilt(bm,am,dip(n3(k):n3(k)+2*n3_1)).*pi/180;
+[pk,nk]=findpeaks(th3f);
+%Here the code checks to find the new peak of the signal. If the index of the first peak is greater than 10
+%then the code sets the first peak as the first index of the signal.
+
+if (isempty(nk) || nk(1)>=10)
+    nk=[1 0];
+end
+%Here the code checks if the indices of the peaks are in a matrix form and if not then 
+%a matrix is created
+if ~ismatrix(nk)
+    nk=[nk 0];
+end
+wn=obw(dip(n3(k):n3(k)+2*n3_1),fs);
 %Starting point for underdamped case
-x0=[th3(n3(k)+n3_1-1) 20 th3(n3(k))-th3(n3(k)+n3_1-1) 0.2 30];
-x01=[th3(n3(k)+n3_1-1) 20 th3(n3(k))-th3(n3(k)+n3_1-1)];
-[Fit,GoF]=fit(transpose(t3),th3(n3(k):n3(k)+n3_1-1),fitfun,'MaxFunEvals',10^6,'MaxIter',10^6,'StartPoint',x0,'ToLFun',10^-8);
-[Fit1,GoF1]=fit(transpose(t3),th3(n3(k):n3(k)+n3_1-1),fitfun1,'MaxFunEvals',10^6,'MaxIter',10^6,'StartPoint',x01,'ToLFun',10^-8);
-[Fit2,GoF2]=fit(transpose(t3),th3(n3(k):n3(k)+n3_1-1),fitfun2,'MaxFunEvals',10^6,'MaxIter',10^6,'StartPoint',x01,'ToLFun',10^-8);
+x0=[th3f(nk(1)+n3_1-1) 20 th3f(nk(1))-th3f(nk(1)+n3_1-1) 0.2 30];
+x01=[th3f(nk(1)+n3_1-1) 20 th3f(nk(1))-th3f(nk(1)+n3_1-1)];
+[Fit,GoF]=fit(transpose(t3),th3f(nk(1):nk(1)+n3_1-1),fitfun,'MaxFunEvals',10^6,'MaxIter',10^6,'StartPoint',x0,'ToLFun',10^-8);
+[Fit1,GoF1]=fit(transpose(t3),th3f(nk(1):nk(1)+n3_1-1),fitfun1,'MaxFunEvals',10^6,'MaxIter',10^6,'StartPoint',x01,'ToLFun',10^-8);
+[Fit2,GoF2]=fit(transpose(t3),th3f(nk(1):nk(1)+n3_1-1),fitfun2,'MaxFunEvals',10^6,'MaxIter',10^6,'StartPoint',x01,'ToLFun',10^-8);
+else 
+    nk=[n3(k) 0];
+    wn=20;
+    %Starting point for underdamped case
+x0=[th3(nk(1)+n3_1-1) 20 th3(nk(1))-th3(nk(1)+n3_1-1) 0.2 30];
+x01=[th3(nk(1)+n3_1-1) 20 th3(nk(1))-th3(nk(1)+n3_1-1)];
+[Fit,GoF]=fit(transpose(t3),th3(nk(1):nk(1)+n3_1-1),fitfun,'MaxFunEvals',10^6,'MaxIter',10^6,'StartPoint',x0,'ToLFun',10^-8);
+[Fit1,GoF1]=fit(transpose(t3),th3(nk(1):nk(1)+n3_1-1),fitfun1,'MaxFunEvals',10^6,'MaxIter',10^6,'StartPoint',x01,'ToLFun',10^-8);
+[Fit2,GoF2]=fit(transpose(t3),th3(nk(1):nk(1)+n3_1-1),fitfun2,'MaxFunEvals',10^6,'MaxIter',10^6,'StartPoint',x01,'ToLFun',10^-8);
+end
+
+
 GoF_rmse=min([GoF.rmse GoF1.rmse GoF2.rmse]);
 
 if GoF_rmse==GoF.rmse
-
+%disp("Underdamped Case");
 Ydip(:,k)=Fit(t3);
 coef=coeffvalues(Fit);
 Bdip(k)=2*Idip*coef(2);
@@ -247,30 +494,38 @@ Kdip(k)=eval(solve(eqn,K));
 theqdip(k)=coef(1);
 th0dip(k)=coef(3)+theqdip(k);
 figure(j+i+k-2)
-plot(t3,Fit(t3).*180/pi,t3,th3(n3(k):n3(k)+n3_1-1).*180/pi);
-legend("Fitted curve","Data from index "+n3(k));
+if wn>20
+    plot(t3,Fit(t3).*180/pi,t3,th3f(nk(1):nk(1)+n3_1-1).*180/pi);
+else
+plot(t3,Fit(t3).*180/pi,t3,th3(nk(1):nk(1)+n3_1-1).*180/pi);
+end
+legend("Fitted curve with cut off "+wn+" Hz","Data from index "+nk(1));
 xlabel("Time (s)");
 ylabel("Angle (degrees)");
 title("DIP joint fit with RMSE: "+(GoF.rmse*180/pi)+" and R^2: "+GoF.rsquare);
 elseif GoF_rmse==GoF1.rmse
-   
+   % disp("Overdamped Case");
     Ydip(:,k)=Fit1(t3);
 coef=coeffvalues(Fit1);
 b=coef(2);
 c=coef(3);
-rf=b*c/(c+(coef(1)-th3(n3(k))));
+rf=b*c/(c+(coef(1)-th3(nk(1))));
 Kdip(k)=rf*b*Idip;
 Bdip(k)=abs(rf+b)*Idip;
 theqdip(k)=coef(1);
-th0pip(k)=th3(n3(k));
+th0pip(k)=th3(nk(1));
 figure(j+i+k-2)
-plot(t3,Fit1(t3).*180/pi,t3,th3(n3(k):n3(k)+n3_1-1).*180/pi);
-legend("Fitted curve","Data from index "+n3(k));
+if wn>20
+    plot(t3,Fit(t3).*180/pi,t3,th3f(nk(1):nk(1)+n3_1-1).*180/pi);
+else
+plot(t3,Fit(t3).*180/pi,t3,th3(nk(1):nk(1)+n3_1-1).*180/pi);
+end
+legend("Fitted curve with cut off "+wn+" Hz","Data from index "+nk(1));
 xlabel("Time (s)");
 ylabel("Angle (degrees)");
 title("DIP joint fit with RMSE: "+(GoF1.rmse*180/pi)+" and R^2: "+GoF1.rsquare);
 else
-    
+    %disp("Critically damped case");
     Ydip(:,k)=Fit2(t3);
     coef=coeffvalues(Fit2);
     Bdip(k)=2*Idip*coef(2);
@@ -280,13 +535,17 @@ else
     theqdip(k)=coef(1);
 th0dip(k)=th3(n3(k));
 figure(j+i+k-2)
-plot(t3,Fit2(t3).*180/pi,t3,th3(n3(k):n3(k)+n3_1-1).*180/pi);
-legend("Fitted curve","Data from index "+n3(k));
+if wn>20
+    plot(t3,Fit(t3).*180/pi,t3,th3f(nk(1):nk(1)+n3_1-1).*180/pi);
+else
+plot(t3,Fit(t3).*180/pi,t3,th3(nk(1):nk(1)+n3_1-1).*180/pi);
+end
+legend("Fitted curve with cut off "+wn+" Hz","Data from index "+nk(1));
 xlabel("Time (s)");
 ylabel("Angle (degrees)");
 title("DIP joint fit with RMSE: "+(GoF2.rmse*180/pi)+" and R^2: "+GoF2.rsquare);
 end
-clear Fit Fit1 Fit2 GoF GoF1 GoF2;
+clear Fit Fit1 Fit2 GoF GoF1 GoF2 bm am;
 end
 
 delete(f);
@@ -316,7 +575,7 @@ x01=[tha(na(m)+nab-1) 20 tha(na(m))-tha(na(m)+nab-1)];
 GoF_rmse=min([GoF.rmse GoF1.rmse GoF2.rmse]);
 
 if GoF_rmse==GoF.rmse
-   
+    %disp("Lowest RMSE underdamped case");
 Ya(:,m)=Fit(ta);
 coef=coeffvalues(Fit);
 Ba(m)=2*Ia*coef(2);
@@ -324,7 +583,7 @@ syms K
 eqn={abs(coef(5))==sqrt(4*K*Ia-Ba(m)^2)/(2*Ia)};
 Ka(m)=eval(solve(eqn,K));
 theqa(m)=coef(1);
-th0a(m)=coef(3)+theqdip(m);
+th0a(m)=coef(3)+theqa(m);
 figure(j+i+k+m-3)
 plot(ta,Fit(ta).*180/pi,ta,tha(na(m):na(m)+nab-1).*180/pi);
 legend("Fitted curve","Data from index "+na(m));
@@ -332,7 +591,7 @@ xlabel("Time (s)");
 ylabel("Angle (degrees)");
 title("Abduction movement fit with RMSE: "+(GoF.rmse*180/pi)+" and R^2: "+GoF.rsquare);
 elseif GoF_rmse==GoF1.rmse
-    
+    %disp("Lowest RMSE overdamped case");
         Ya(:,m)=Fit1(ta);
         coef=coeffvalues(Fit1);
         b=coef(2);
@@ -349,7 +608,7 @@ xlabel("Time (s)");
 ylabel("Angle (degrees)");
 title("Abduction movement fit with RMSE: "+(GoF1.rmse*180/pi)+" and R^2: "+GoF1.rsquare);
 else
-       
+        %disp("Lowest RMSE critically damped case");
         Ya(:,m)=Fit2(ta);
         coef=coeffvalues(Fit2);
         b=coef(2);
@@ -379,3 +638,5 @@ disp("Mean DIP K: "+mean(Kdip)+" +/- "+std(Kdip));
 disp("Mean DIP B: "+mean(Bdip)+" +/- "+std(Bdip));
 disp("Mean Abduction K: "+mean(Ka)+" +/- "+std(Ka));
 disp("Mean Abduction B: "+mean(Ba)+" +/- "+std(Ba));
+
+end
